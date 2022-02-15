eaa518468de312630fbe07e62ca67d28c7be91e0
gabriela lacerda <gabriela.lacerda2003@outlook.com>
2022-02-15 04:26:21 -0300

    nova lista p1

--- lista2/n5.cpp
+++ lista2/n5.cpp
@@ -1,104 +1,47 @@
+//o maior salario de cada departamento, a soma dos salarios de cada departamento e a soma de todos os salarios da empresa
 #include <iostream>
 
 using namespace std;
 
+int departamento(int quant_funcionarios){
 
-int empresa(int quant_funcionarios){
+int matricula,i=1;
+float soma_salarioDep=0;
+float salario=0,maior=0;
 
-float i,maiorsalarioDep=0,salariototalDep=0,salariototalEmp=0,salarioDep;
+while(i<=quant_funcionarios){
 
-for(i=1;i<=quant_funcionarios;i++){
- cout << "digite aqui seu salario:\n";
-        cin >> salarioDep;
+  cout << "funcionario " << i << ", por favor,entre com a matricula : \n" ;
+  cin >> matricula;
+  cout  << "entre tambem com o salario : \n";
+  cin >> salario;
+  
+      (salario>=maior) ? maior=salario : ;
+
+i++;
+soma_salarioDep += salario++;
 }
-   if(salarioDep>=maiorsalarioDep){
-      maiorsalarioDep = salarioDep;   
-salariototalDep=salarioDep++;
+cout << "o maior salario da empresa e : \n" << maior << "\n";
+
+return soma_salarioDep;    
 }
- cout << "o maior salario do departamento : " << maiorsalarioDep << "\n";   
-      cout << "salario total do departamento : " << salariototalDep<< "\n";
-
-
-//    salariototalDep+=salarioDep onde eu coloco?;
-  // cout << "o salario total do departamento " << salariototalDep << "\n";  
-  // cout << "o salario total da empresa: " << salariototalEmp << "\n";
-
-return 0;
-}
-
 int main(){
 
-int quant_departamento = 1;
-int i=1,j=3,matricula,sexo,quant_funcionarios=1;
-float salarioDep,maiorsalarioDep,salariototalDep=0,salariototalEmp=0;
+int quant_funcionarios=2,j=1;
+float SALARIO_TOTAL_EMPRESA = 0;
 
+while(j<=10){
 
-  while(quant_departamento<=j){
-  cout <<  "digite a quantidade de funcionarios no departamento " << quant_departamento << " da empresa\n";
-                 
-  cin >> quant_funcionarios;
-      while(i<=quant_funcionarios){
-        cout << "digite aqui sua matricula:\n";
-        cin >> matricula;
-        cout << "digite aqui seu sexo:\n" << "(1 - feminino 2 - masculino)\n";
-        cin >> sexo;
-        cout << empresa(quant_funcionarios);
-      }
-    quant_departamento++;
-  }
+ cout << "\n";
+ cout << "departamento " << j << "\n";
+ cout << "\n";
+ SALARIO_TOTAL_EMPRESA += departamento(quant_funcionarios);
 
-
-
-
-return 0;
+j++;
 }
-// #include <iostream>
 
-// using namespace std;
+cout << "a soma de todos os salarios da empresa : " << SALARIO_TOTAL_EMPRESA << "\n";
+cout << "\n";
 
-
-// int empresa(int quant_departamento){
-
-// int i,j,matricula,sexo,quant_funcionarios;
-// float salarioDep,maiorsalarioDep,salariototalDep=0,salariototalEmp=0;
-
-// for(i=1;i<=quant_departamento;i++){
-
-//   cout <<  "digite a quantidade de funcionarios no departamento " << i << " da empresa\n";
-//   cin >> quant_funcionarios;
-
-//     for(j=1;j<=quant_funcionarios;j++){
-
-//         cout << "digite aqui sua matricula:\n";
-//         cin >> matricula;
-//         cout << "digite aqui seu sexo:\n" << "(1 - feminino 2 - masculino)\n";
-//         cin >> sexo;
-//         cout << "digite aqui seu salario:\n";
-//         cin >> salarioDep;
-//                     salariototalEmp+=salarioDep;
-
-//             if(salarioDep>=maiorsalarioDep){
-
-//                  maiorsalarioDep = salarioDep;
-//                  cout << "o maior salario do departamento " << i << " : " << maiorsalarioDep <<     "\n";     
-
-//             }
-//         }
-//     }    
-
-// //    salariototalDep+=salarioDep onde eu coloco?;
-
-//   cout << "o salario total do departamento " << salariototalDep << "\n";  
-//   cout << "o salario total da empresa: " << salariototalEmp << "\n";
-
-// return 0;
-// }
-
-// int main(){
-
-// int quant_departamento = 2;
-
-// cout << empresa(quant_departamento)<< "\n";
-
-// return 0;
-// }
+return 0; 
+}
---
+++ lista2/n9.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int horista(){
+
+float valor_hora_trabalho,quant_horas_trabalhadas,salario=0;
+
+    cout << "por favor, digite aqui o valor da hora de trabalho : \n";
+    cin >> valor_hora_trabalho;
+    cout << "agora o valor de horas  trabalhadas: \n";
+    cin >> quant_horas_trabalhadas;
+
+salario = valor_hora_trabalho * quant_horas_trabalhadas;
+
+
+return salario;
+}
+int contratado(){ 
+
+float salario=0;
+
+    cout << "por favor, digite aqui seu salario: \n";
+    cin >> salario;
+    cout << "\n";
+
+return salario; 
+}
+int prestador_de_serviço(){
+
+float valor_servico,quant_serv_prestados,salario=0;
+
+    cout << "por favor, digite aqui a quantidade de servicos prestados: \n";
+    cin >> quant_serv_prestados;
+    cout << "agora o valor para cada serviço: \n";
+    cin >> valor_servico;
+    cout << "\n";   
+
+salario = quant_serv_prestados * valor_servico;
+
+return salario;
+}
+int menu(){
+
+int i=1,funcao;
+
+while(i<=20){
+
+cout << "\n";
+cout << "em qual desses menu's voce se encaixa? \n";
+cout << "1 - HORISTA 2 - CONTRATADO 3 - PRESTADOR DE SERVICO: \n";
+cin >> funcao;
+
+  switch (funcao)
+  {
+  case 1: 
+      cout << horista();
+      break; 
+  case 2:
+      cout << contratado();
+      break;
+  case 3:
+      cout << prestador_de_serviço();
+      break;
+  default:
+      break;
+  }
+
+i++;    
+}
+return funcao;
+}
+int main(){
+
+cout << menu();
+cout << "\n";
+
+return 0;    
+}
